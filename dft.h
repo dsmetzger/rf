948cd3133ae52779b8c23f644d77b0104e6a4dfe
dsmetzger <dylan.s.metzger@gmail.com>
2020-01-11 14:25:46 -0500

    Update dft.h

--- dft.h
+++ dft.h
@@ -6,10 +6,13 @@ Library for discrete fourier transform algorithims. Everything should be either 
 #include <vector>
 
 
-void cas(float m_rad){
-    
+float cas(float m_rad){
+    const float c1 = 1.41421356237;
+    const float c2 = 0.78539816339;
+    return c1*cos(m_rad-c2);
 }
 
+/*
 //class LUT
 class casLUT{
 private:
@@ -17,21 +20,43 @@ private:
 public:
 
 }
+*/
 
-
-//DHT functions
-void brute_dht(short* x, float* Xk, const int N){
-    double factor = 2.0*M_PI/double(N);
-    double factor1;
-    for (int k=0; k<N; ++k){
-        factor1 = factor*k;
-        Xk[k] = 0;
-        for (int n=0; n<N; ++n){
-            Xk[k]+= x[n]*cas(factor1*n)
+class DHT{
+private:
+    std::vector<float> casLUT;
+    int N
+public:
+    DHT(int N):N(N){
+        casLUT.resize(N*N);
+        int idx = 0;
+        double factor = 2.0*M_PI/double(N);
+        double factor1;
+        for (int k=0; k<N; ++k){
+            factor1 = factor*k;
+            for (int n=0; n<N; ++n){
+                casLUT[idx]= cas(factor1*n);
+                ++idx;
+            }
         }
+    }
+    
+    void brute_dht(float* x, float* Xk){
+        int idx = 0;
+        for (int k=0; k<N; ++k){
+            Xk[k] = 0;
+            for (int n=0; n<N; ++n){
+                Xk[k]+= x[n]*casLUT[idx];
+                ++idx;
+            }
+        }
+    }
+    
+    void convolve_dht(){
+        
     }
 }
 
-void convolve_dht(){
 
-}
+
+
