0fc98881e478adf903b96ff41d8ce73b1dd96b47
dsmetzger <dylan.s.metzger@gmail.com>
2020-01-11 14:43:15 -0500

    Create generate_fft.py

---
+++ generate_fft.py
@@ -0,0 +1,24 @@
+#Xk =  sum 0:N-1(xn*e^-i*2*pi*k*n/N)
+
+import math
+
+N=4096
+
+
+#section off by k*n%N and (xn*e^-i*2*pi*k*n/N)
+exp_strs = []
+
+for n in range 0:N:
+    exp_dict[n] = ""
+
+
+
+
+
+
+
+
+print "void fft_"+str(N)+"(){"
+
+
+print "}"
