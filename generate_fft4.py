df60c9abc726bd77f96bb4713f0a0a26d7fcd4ae
dsmetzger <dylan.s.metzger@gmail.com>
2020-01-14 16:50:34 -0500

    Update generate_fft4.py

--- generate_fft4.py
+++ generate_fft4.py
@@ -3,14 +3,28 @@
 
 import math
 
-N=4096
+N=32
 
 
 print "void fft_"+str(N)+"(float* x, float* X){"
-layer = 1
+
+layer = 0
+layers = [[]]
+
+step = 1
 while(N!=1):
-    
+    if (layer ==0):
+        for k in range(0,N):
+            layers[layer].append(k)
+            #print "X["+str(k)+"]=x["+str(k)+"];"
+    else:
+        layers.append([])
+        for x in range(0,step):
+            for k in range(0,N,step):
+                layers[layer].append(layers[layer-1][])
     layer+=1
+    N/=2
+    step*=2
 
 
 print "}"
